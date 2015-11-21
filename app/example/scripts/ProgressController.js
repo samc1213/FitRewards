530f8a9fdd76eeda8c8d45c21595e19fed97f0ee
sophialou <sophialou@users.noreply.github.com>
2015-11-21 15:13:34 -0600

    Update ProgressController.js

--- app/example/scripts/ProgressController.js
+++ app/example/scripts/ProgressController.js
@@ -1,7 +1,23 @@
 angular
   .module('example')
   .controller('ProgressController', function($scope, supersonic) {
-
     $scope.navbarTitle = "Progress";
+    $scope.user = {};
+    $scope.user.progress = 0;
 
+        supersonic.ui.views.current.whenVisible(function() {
+          var GameScore = Parse.Object.extend("Goals");
+		  var query = new Parse.Query(GameScore);
+		 query.equalTo("userid", "mX6kUboBzf");
+		 query.find({
+		   success: function(results) {
+		
+		     $scope.user.currentgoal = results.length;
+		 },
+		  error: function(error) {
+		  
+		  }
+		 });
+        });
+    
   });
