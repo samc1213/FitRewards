381f3a4bb15baeb9bc465d0899c7ec01344339be
Edwin Wu <edwinwu2017@u.northwestern.edu>
2015-11-22 04:05:38 -0600

    Show Amazon items

--- app/example/scripts/GoalSetController.js
+++ app/example/scripts/GoalSetController.js
@@ -3,15 +3,41 @@ angular
   .controller('GoalSetController', function($scope, supersonic, $http) {
     $scope.navbarTitle = "Goal Set!";
     $scope.user = {};
+    $scope.amazonitems = [];
+    $scope.selectedamazonitem = {};
+    $scope.selectedamazonitem.poop = {};
+    $scope.user = {checker:false};
+    $scope.user = {checker2: false};
+
 
    //  $scope.$watch('myVar', function() {
    //     alert('hey, myVar has changed!');
    // });
 
+  $scope.clickee = function(index){
+    $scope.sel = false;
+    $scope.selectedamazonitem.poop = $scope.amazonitems[index];
+    $scope.user.checker = true;
+
+    var modalView = new supersonic.ui.View("example#popup");
+    var options = {
+      animate: true,
+      params: {
+        id: $scope.amazonitems[index]
+      }
+    }
+
+    supersonic.ui.modal.show(modalView, options);
+
+  }
+
 	$scope.searchAmazon = function() {
 		supersonic.logger.log("IN searchAmazon");
 		var query = $scope.user.amazonquery;
-
+    $scope.amazonitems = [];
+    $scope.selectedamazonitem.poop = {};
+    $scope.sel = false;
+    $scope.user.checker = false;
 		$http({
             method: 'POST',
             url: 'https://floating-bastion-3464.herokuapp.com/amazon.php',
@@ -24,7 +50,28 @@ angular
              },
 	        data: {keyword: query}
 	        }).then(function successCallback(response) {
-	            supersonic.logger.log("response " + response.data);
+              $scope.sel = true;
+              for (var i = 0; i < response.data.length; i++){
+                var item = {};
+                item.title = response.data[i].title;
+                item.image = response.data[i].image;
+                // supersonic.logger.log(item.image);
+                var fullnum = response.data[i].price;
+                // supersonic.logger.log(typeof fullnum);
+
+                var beforenum = fullnum.substring(0, fullnum.length-2);
+                // supersonic.logger.log(beforenum);
+                var afternum = fullnum.substring(fullnum.length-2);
+                // supersonic.logger.log(afternum);
+
+                item.price = "$" + beforenum + "." + afternum;
+                // supersonic.logger.log(item.price);
+                item.chosen = false;
+
+                item.asin = response.data[i].asin;
+                $scope.amazonitems.push(item);
+              }
+              supersonic.logger.log($scope.amazonitems);
 	        }, function errorCallback(response) {
 	            supersonic.logger.log(response);
 	        });
